4dee24215c51cec10fd4a73507bee17a085cc65d
kseniyaav <123232274+kseniyaav@users.noreply.github.com>
2023-01-24 15:33:31 +0300

    Revert "Develop"

--- str_func.py
+++ str_func.py
@@ -6,13 +6,3 @@ def make_capital_letters():
     print(s.upper())
 
 make_capital_letters()
-
-
-def make_first_letter_capital():
-    '''
-    Функция принимает на вход строку и делает заглавными первые буквы каждого слова в строке
-    '''
-    s = input("Введите текст ")
-    print(s.title())
-make_first_letter_capital()
-
